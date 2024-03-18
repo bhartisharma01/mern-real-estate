d34a9c3cd60bdc441b3c19ea96678e01c7253ef5
Bharti Sharma <sharma.bharti1510@gmail.com>
2024-03-18 15:11:17 +0530

    added create listing api route

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import path from 'path';
 
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import listingRouter from './routes/listing.route.js'
 import cookieParser from "cookie-parser";
 
 
@@ -30,7 +31,8 @@ app.listen(3000, ()=>{
 });
 
 app.use('/api/user', userRouter);
-app.use('/api/auth', authRouter)
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter)
 
 app.use((err, req, res, next)=>{
     const statusCode=err.statusCode || 500;
