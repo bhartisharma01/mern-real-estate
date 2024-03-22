4bc2473716a5da38b62c8bfc3f4c547cd53d4aec
Bharti Sharma <sharma.bharti1510@gmail.com>
2024-03-22 13:04:31 +0530

    Complete header search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Header() {
   const {currentUser} = useSelector((state)=> state.user);
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm)
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+
+  }
+
+  useEffect(() => {
+  const urlParams = new URLSearchParams(location.search);
+  const searchTermFromUrl = urlParams.get('searchTerm');
+  if(searchTermFromUrl){
+    setSearchTerm(searchTermFromUrl);
+  }
+  }, [location.search])
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex items-center flex-wrap justify-between mx-auto max-w-6xl p-3">
@@ -17,15 +35,17 @@ function Header() {
           </Link>
         </div>
         <div>
-          <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+          <form onSubmit={handleSubmit} className="bg-slate-100 p-3 rounded-lg flex items-center">
             <input
               type="text"
               name=""
               id=""
               placeholder="Search..."
+              value={searchTerm}
               className="bg-transparent focus:outline-none w-24 sm:w-64"
+              onChange={(e)=> setSearchTerm(e.target.value)}
             />
-            <FaSearch className="text-slate-600" />
+           <button> <FaSearch className="text-slate-600" /></button>
           </form>
         </div>
         <div>
