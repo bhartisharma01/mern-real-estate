0cc9e2cf950c510c3950aca399a7f64997925338
Bharti Sharma <sharma.bharti1510@gmail.com>
2024-03-27 05:51:36 +0530

    width of the listing cards

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -3,7 +3,7 @@ import { MdLocationOn } from "react-icons/md";
 
 function ListingItem({ listing }) {
   return (
-    <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[420px]">
+    <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[365px]">
       <Link to={`/listing/${listing._id}`}>
         <img
           src={
