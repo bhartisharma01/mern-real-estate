e1013bddae7da3f47a77cf6c2f6d65012d09d660
Bharti Sharma <sharma.bharti1510@gmail.com>
2024-03-27 15:41:00 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -10,10 +10,11 @@ import cookieParser from "cookie-parser";
 
 
 const __filename = new URL(import.meta.url).pathname;
-const __dirname = path.dirname(__filename);
+// const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
 let envPath=path.join(__dirname,'../','.env');
 dotenv.config({ path: envPath });
-
+console.log("checming dirname ", __dirname);
 // db connection
  mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log("mongo db connected")
@@ -33,6 +34,12 @@ app.listen(3000, ()=>{
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter)
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 
 app.use((err, req, res, next)=>{
     const statusCode=err.statusCode || 500;
